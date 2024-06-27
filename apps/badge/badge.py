7dbdafa00e839a89e6b20f2757d2f4609b2adff0
Gabriel Knoy <gabriel.knoy@gmail.com>
2024-06-26 23:23:32 -0700

    Refactor+dedup badge data files, refactor badge rendering

--- apps/badge/badge.py
+++ apps/badge/badge.py
@@ -1,16 +1,16 @@
 """
 # badge.py: Refactored app for simpler loading in the launcher
-# (For now layout of items is basically the same as the example.)
+# (For now layout + sizes of items is basically the same as the example.)
 # Badge config format:
 #   {
-#       "topHeading": "company",
+#       "heading": "company",
 #       "name": "name",
 #       "r1_title": "title 1",
 #       "r1_text": "text 1",
 #       "r2_title": "title 2",
 #       "r2_text": "text 2",
 #       "image": {
-#           "file": "badge.png", // png or jpg
+#           "file": "badge.jpg",  // no png sadly
 #           "width": 135,  // pixels
 #       }
 #   }
@@ -33,31 +33,29 @@ from apps.app_base import AppBase
 WIDTH = badger2040.WIDTH
 HEIGHT = badger2040.HEIGHT
 
+BLACK = 0
+WHITE = 15
+
+# FIXME: read from data
 # IMAGE_WIDTH = 104
 IMAGE_WIDTH = 128
 
-COMPANY_HEIGHT = 30
+# FIXME: get rid of field-specific globals constants
+HEADING_HEIGHT = 30
 DETAILS_HEIGHT = 20
-NAME_HEIGHT = HEIGHT - COMPANY_HEIGHT - (DETAILS_HEIGHT * 2) - 2
+NAME_HEIGHT = HEIGHT - HEADING_HEIGHT - (DETAILS_HEIGHT * 2) - 2
 TEXT_WIDTH = WIDTH - IMAGE_WIDTH - 1
 
-COMPANY_TEXT_SIZE = 0.6
+HEADING_TEXT_SIZE = 0.6
 DETAILS_TEXT_SIZE = 0.5
 
 LEFT_PADDING = 5
 NAME_PADDING = 20
 DETAIL_SPACING = 10
 
-BADGE_PATH = "/badges/badge.txt"
+# FIXME: list the json files
+BADGE_PATH = "/apps/badge/data/gknoy.json"
 
-DEFAULT_TEXT = """Myriad Genetics
-Gabriel Knoy
-gknoy@myriad.com
-
-Data Review Engineering
-
-/badges/kshot-badge-128.jpg
-"""
 
 # ------------------------------
 #      Utility functions
@@ -65,7 +63,7 @@ Data Review Engineering
 
 
 # Reduce the size of a string until it fits within a given width
-def truncatestring(text, text_size, width):
+def truncatestring(display, text: str, text_size: float, width: int):
     while True:
         length = display.measure_text(text, text_size)
         if length > 0 and length > width:
@@ -80,8 +78,94 @@ def truncatestring(text, text_size, width):
 # ------------------------------
 
 
+def scale_font_to_fit_width(display, text: str, size: float, max_width: int):
+    # Find the font size that lets text fit within a width
+    # assumes that we've already done display.set_font(...)
+    font_size = size if size >= 0.1 else 2.0
+    text_width = display.measure_text(text, font_size)
+    while text_width > max_width and font_size >= 0.1:
+        font_size -= 0.01
+        text_width = display.measure_text(text, font_size)
+    return font_size
+
+
+# def _scale2(display, text: str, size: float, max_width: int):
+#     font_size = size if size >= 0.1 else 2.0
+#     while True:
+#         text_width = display.measure_text(text, font_size)
+#         if text_width >= max_width and font_size >= 0.1:
+#             font_size -= 0.01
+#         else:
+#             return font_size
+
+
+def render_text(
+    display,  # picographics instance e.g. Badger2040().display
+    text: str,
+    x: int,
+    y: int,
+    width: int,
+    text_size: float,
+    color: int,
+    font: str | None = None,
+    scale_font=False,
+    max_text_width=WIDTH,
+):
+    display.set_pen(color)  # Change this to 0 if a white background is used
+    if font is not None:
+        display.set_font(font)
+    if scale_font:
+        text_size = scale_font_to_fit_width(display, text, text_size, max_text_width)
+    display.text(text, x, y, width, text_size)
+
+
+def render_text_drop_shadow(
+    display,  # picographics instance e.g. Badger2040().display
+    text: str,
+    x: int,
+    y: int,
+    width: int,
+    text_size: float,
+    fg: int,
+    bg: int,
+    font: str | None = None,
+    scale_font=False,
+    max_text_width=WIDTH,
+    offsets: list[int] = None,
+):
+    if offsets is None:
+        offsets = [1]
+    for offset in offsets:
+        render_text(
+            display,
+            text,
+            x + offset,
+            y + offset,
+            width,
+            text_size,
+            bg,
+            font,
+            scale_font,
+            max_text_width,
+        )
+    render_text(
+        display, text, x, y, width, text_size, fg, font, scale_font, max_text_width
+    )
+
+
 # Draw the badge, including user text
-def draw_badge():
+# FIXME: parametrize with data
+def draw_badge(
+    display,
+    jpeg,
+    heading: str,
+    name: str,
+    detail1_title: str,
+    detail1_text: str,
+    detail2_title: str,
+    detail2_text: str,
+    badge_image: str,  # todo: supportm multiple images?
+):
     display.set_pen(0)
     display.clear()
 
@@ -96,38 +180,52 @@ def draw_badge():
     display.line(WIDTH - IMAGE_WIDTH, HEIGHT - 1, WIDTH - 1, HEIGHT - 1)
     display.line(WIDTH - 1, 0, WIDTH - 1, HEIGHT - 1)
 
-    # Uncomment this if a white background is wanted behind the company
-    # display.set_pen(15)
-    # display.rectangle(1, 1, TEXT_WIDTH, COMPANY_HEIGHT - 1)
+    # heading background
+    display.set_pen(BLACK)
+    display.rectangle(1, 1, TEXT_WIDTH, HEADING_HEIGHT - 1)
 
     # Draw the company
-    display.set_pen(15)  # Change this to 0 if a white background is used
-    display.set_font("serif")
-    display.text(
-        company, LEFT_PADDING, (COMPANY_HEIGHT // 2) + 1, WIDTH, COMPANY_TEXT_SIZE
+    render_text_drop_shadow(
+        display=display,
+        text=heading,
+        x=LEFT_PADDING,
+        y=(HEADING_HEIGHT // 2) + 1,
+        width=WIDTH,
+        text_size=HEADING_TEXT_SIZE,  # 0.6 !?
+        fg=BLACK,
+        bg=WHITE,
     )
+    # display.set_pen(15)  # Change this to 0 if a white background is used
+    # display.set_font("serif")
+    # display.text(
+    #     company, LEFT_PADDING, (COMPANY_HEIGHT // 2) + 1, WIDTH, COMPANY_TEXT_SIZE
+    # )
 
     # Draw a white background behind the name
-    display.set_pen(15)
-    display.rectangle(1, COMPANY_HEIGHT + 1, TEXT_WIDTH, NAME_HEIGHT)
+    display.set_pen(WHITE)
+    display.rectangle(1, HEADING_HEIGHT + 1, TEXT_WIDTH, NAME_HEIGHT)
 
     # Draw the name, scaling it based on the available width
-    display.set_pen(0)
-    display.set_font("sans")
-    name_size = 2.0  # A sensible starting scale
-    while True:
-        name_length = display.measure_text(name, name_size)
-        if name_length >= (TEXT_WIDTH - NAME_PADDING) and name_size >= 0.1:
-            name_size -= 0.01
-        else:
-            display.text(
-                name,
-                (TEXT_WIDTH - name_length) // 2,
-                (NAME_HEIGHT // 2) + COMPANY_HEIGHT + 1,
-                WIDTH,
-                name_size,
-            )
-            break
+    # name_size = scale_font_to_fit_width(display, name, 2.0, (TEXT_WIDTH - NAME_PADDING))
+    # name_len = display.measure_text(name, DETAILS_TEXT_SIZE)
+    render_text_drop_shadow(
+        display=display,
+        text=name,
+        # FIXME: text centering
+        x=NAME_PADDING,  # (TEXT_WIDTH - name_len) // 2,
+        y=(NAME_HEIGHT // 2) + HEADING_HEIGHT + 1,
+        width=WIDTH,
+        text_size=2.0,
+        fg=BLACK,
+        bg=WHITE,
+        font="sans",
+        scale_font=True,
+        max_text_width=(TEXT_WIDTH - 2 * NAME_PADDING),
+        offsets=[1, -1],
+    )
+    # display.set_pen(0)
+    # display.set_font("sans")
+    # name_size = 2.0  # A sensible starting scale
 
     # Draw a white backgrounds behind the details
     display.set_pen(15)
@@ -135,39 +233,60 @@ def draw_badge():
     display.rectangle(1, HEIGHT - DETAILS_HEIGHT, TEXT_WIDTH, DETAILS_HEIGHT - 1)
 
     # Draw the first detail's title and text
-    display.set_pen(0)
+
+    # display.set_pen(0)
     display.set_font("sans")
-    name_length = display.measure_text(detail1_title, DETAILS_TEXT_SIZE)
-    display.text(
-        detail1_title,
-        LEFT_PADDING,
-        HEIGHT - ((DETAILS_HEIGHT * 3) // 2),
-        WIDTH,
-        DETAILS_TEXT_SIZE,
+    d1_title_len = display.measure_text(detail1_title, DETAILS_TEXT_SIZE)
+    render_text_drop_shadow(
+        display=display,
+        text=detail1_title,
+        x=LEFT_PADDING,
+        y=HEIGHT - ((DETAILS_HEIGHT * 3) // 2),
+        width=WIDTH,
+        text_size=DETAILS_TEXT_SIZE,
+        fg=BLACK,
+        bg=WHITE,
+        font="sans",
+        offsets=[1, -1],
     )
-    display.text(
-        detail1_text,
-        5 + name_length + DETAIL_SPACING,
-        HEIGHT - ((DETAILS_HEIGHT * 3) // 2),
-        WIDTH,
-        DETAILS_TEXT_SIZE,
+    render_text_drop_shadow(
+        display=display,
+        text=detail1_text,
+        x=5 + d1_title_len + DETAIL_SPACING,
+        y=HEIGHT - ((DETAILS_HEIGHT * 3) // 2),
+        width=WIDTH,
+        text_size=DETAILS_TEXT_SIZE,
+        fg=BLACK,
+        bg=WHITE,
+        font="sans",
+        offsets=[1, -1],
     )
 
     # Draw the second detail's title and text
-    name_length = display.measure_text(detail2_title, DETAILS_TEXT_SIZE)
-    display.text(
-        detail2_title,
-        LEFT_PADDING,
-        HEIGHT - (DETAILS_HEIGHT // 2),
-        WIDTH,
-        DETAILS_TEXT_SIZE,
+    d2_title_len = display.measure_text(detail2_title, DETAILS_TEXT_SIZE)
+    render_text_drop_shadow(
+        display=display,
+        text=detail2_title,
+        x=LEFT_PADDING,
+        y=HEIGHT - (DETAILS_HEIGHT // 2),
+        width=WIDTH,
+        text_size=DETAILS_TEXT_SIZE,
+        fg=BLACK,
+        bg=WHITE,
+        font="sans",
+        offsets=[1, -1],
     )
-    display.text(
-        detail2_text,
-        LEFT_PADDING + name_length + DETAIL_SPACING,
-        HEIGHT - (DETAILS_HEIGHT // 2),
-        WIDTH,
-        DETAILS_TEXT_SIZE,
+    render_text_drop_shadow(
+        display=display,
+        text=detail2_text,
+        x=LEFT_PADDING + d2_title_len + DETAIL_SPACING,
+        y=HEIGHT - (DETAILS_HEIGHT // 2),
+        width=WIDTH,
+        text_size=DETAILS_TEXT_SIZE,
+        fg=BLACK,
+        bg=WHITE,
+        font="sans",
+        offsets=[1, -1],
     )
 
     display.update()
@@ -185,44 +304,45 @@ display.set_thickness(2)
 
 jpeg = jpegdec.JPEG(display.display)
 
-# Open the badge file
-try:
-    badge = open(BADGE_PATH, "r")
-except OSError:
-    with open(BADGE_PATH, "w") as f:
-        f.write(DEFAULT_TEXT)
-        f.flush()
-    badge = open(BADGE_PATH, "r")
+# FIXME READ/PICK JSON
+# # Open the badge file
+# try:
+#     badge = open(BADGE_PATH, "r")
+# except OSError:
+#     with open(BADGE_PATH, "w") as f:
+#         f.write(DEFAULT_TEXT)
+#         f.flush()
+#     badge = open(BADGE_PATH, "r")
 
-# Read in the next 6 lines
-company = badge.readline()  # "mustelid inc"
-name = badge.readline()  # "H. Badger"
-detail1_title = badge.readline()  # "RP2040"
-detail1_text = badge.readline()  # "2MB Flash"
-detail2_title = badge.readline()  # "E ink"
-detail2_text = badge.readline()  # "296x128px"
-badge_image = badge.readline()  # /badges/badge.jpg
+# # Read in the next 6 lines
+# heading = badge.readline()  # "mustelid inc"
+# name = badge.readline()  # "H. Badger"
+# detail1_title = badge.readline()  # "RP2040"
+# detail1_text = badge.readline()  # "2MB Flash"
+# detail2_title = badge.readline()  # "E ink"
+# detail2_text = badge.readline()  # "296x128px"
+# badge_image = badge.readline()  # /badges/badge.jpg
 
-# Truncate all of the text (except for the name as that is scaled)
-company = truncatestring(company, COMPANY_TEXT_SIZE, TEXT_WIDTH)
+# # Truncate all of the text (except for the name as that is scaled)
+# heading = truncatestring(heading, HEADING_TEXT_SIZE, TEXT_WIDTH)
 
-detail1_title = truncatestring(detail1_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
-detail1_text = truncatestring(
-    detail1_text,
-    DETAILS_TEXT_SIZE,
-    TEXT_WIDTH
-    - DETAIL_SPACING
-    - display.measure_text(detail1_title, DETAILS_TEXT_SIZE),
-)
+# detail1_title = truncatestring(detail1_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
+# detail1_text = truncatestring(
+#     detail1_text,
+#     DETAILS_TEXT_SIZE,
+#     TEXT_WIDTH
+#     - DETAIL_SPACING
+#     - display.measure_text(detail1_title, DETAILS_TEXT_SIZE),
+# )
 
-detail2_title = truncatestring(detail2_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
-detail2_text = truncatestring(
-    detail2_text,
-    DETAILS_TEXT_SIZE,
-    TEXT_WIDTH
-    - DETAIL_SPACING
-    - display.measure_text(detail2_title, DETAILS_TEXT_SIZE),
-)
+# detail2_title = truncatestring(detail2_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
+# detail2_text = truncatestring(
+#     detail2_text,
+#     DETAILS_TEXT_SIZE,
+#     TEXT_WIDTH
+#     - DETAIL_SPACING
+#     - display.measure_text(detail2_title, DETAILS_TEXT_SIZE),
+# )
 
 
 # ------------------------------
@@ -241,7 +361,20 @@ class BadgeApp(AppBase):
 
 
 if __name__ == "/apps/badge/badge":
-    draw_badge()
+    draw_badge(
+        # TODO: read config from a json file
+        display=display,
+        jpeg=jpeg,
+        heading="Myriad Genetics",
+        name="Gabriel Knoy",
+        detail1_title="gknoy@myriad.com",
+        detail1_text="",
+        detail2_title="Data Review Engineering",
+        detail2_text="",
+        # badge_image="/apps/badge/data/portrait-128.jpg",
+        # badge_image="/apps/badge/data/kelaan-portrait-135x128-bw2.jpg",
+        badge_image="/apps/badge/data/gknoy-badge-128.jpg",
+    )
     while True:
         # Sometimes a button press or hold will keep the system
         # powered *through* HALT, so latch the power back on.
